c9ccb3a70f1738f95bb1556631942162275ad33b
athemm <47975908+athemm@users.noreply.github.com>
2020-06-14 01:29:44 +0300

    update for config and 60x60 support

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
+import sys
+import json
+
 import datetime
-
 x = datetime.datetime.now()
-
 file_thing = x.strftime("%Y%m%d%H%M%S")
 
 b_list = []
@@ -25,25 +26,55 @@ bl = dict(
     heist_safe = "12"
 )
 
-out = open(file_thing + ".es3", "w+")
-save = open("saves.es3", "w+")
-save.write('{"saves":{"__type":"System.Collections.Generic.List`1[[MapSave, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]],mscorlib","value":[{"id":"' + file_thing + '","mapName":"' + file_thing + '","mapAuthor":"Anonymous","mapEvent":"Custom"}]}}')
-#  fp = input("enter file name here: ")
 gogo = 0
 cnt = 1
 bro = 0
 indexc = 0
 somec = 0
 
+try:
+    json_file_config = open("config.json", "r+")
+except FileNotFoundError:
+    print("cannot find config file")
+    sys.exit()
+
+conf = json.load(json_file_config) # load the json
+
+# map size stuff
+size = conf["mapSize"]
+
+if size == "1":
+    sizeId = "0"
+    bro_number = 693
+    somec_check = 32
+    gogo_check = 21
+
+elif size == "2":
+    sizeId = "2"
+    bro_number = 3600
+    somec_check = 59
+    gogo_check = 60
+
+else:
+    print("unsupported map size")
+    sys.exit()
+   
+out = open(file_thing + ".es3", "w+")
+save = open("saves.es3", "w+")
+save.write('{"saves":{"__type":"System.Collections.Generic.List`1[[MapSave, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]],mscorlib","value":[{"id":"' + file_thing + '","mapName":"' + conf["mapName"] + '","mapAuthor":"' + conf["mapAuthor"] + '","mapEvent":"Custom"}]}}')
 
 
 out.write('{"IDs":{"__type":"System.Int32[,],mscorlib","value":[')
 
 
-
-
-while bro < 693:
-    f = open("sample.txt", "r+")
+while bro < bro_number:
+    try:
+        f = open(conf["fileName"], "r+")
+    except FileNotFoundError:
+        print("Put your map in a file called 'sample.txt' or change that in config file")
+        input()
+        sys.exit()
+    
     line = f.readline()
     while line:
         if line.strip()[indexc] == "W":
@@ -78,7 +109,7 @@ while bro < 693:
         else:
             b_list.append(bl["no"])
         bro += 1
-        if somec == 32:
+        if somec == somec_check:
             indexc += 1
             somec = 0
             gogo += 1
@@ -86,10 +117,10 @@ while bro < 693:
             for i in range(0, len(b_list)): 
                 b_list[i] = int(b_list[i])
             b_list.reverse()
-            if gogo != 21:
+            if gogo != gogo_check:
                 out.write(str(b_list) + ",")
             else:
-                out.write(str(b_list) + ']},"Event":{"__type":"System.Int32","value":0},"Theme":{"__type":"System.Int32","value":3},"Size":{"__type":"System.Int32","value":0},"Name":{"__type":"System.String","value":"' + file_thing + '"},"Author":{"__type":"System.String","value":"Anonymous"},"ID":{"__type":"System.String","value":"' + file_thing + '"}}')
+                out.write(str(b_list) + ']},"Event":{"__type":"System.Int32","value":0},"Theme":{"__type":"System.Int32","value":3},"Size":{"__type":"System.Int32","value":' + sizeId + '},"Name":{"__type":"System.String","value":"' + conf["mapName"] + '"},"Author":{"__type":"System.String","value":"' + conf["mapName"] + '"},"ID":{"__type":"System.String","value":"' + file_thing + '"}}')
             b_list = []
         else:
             somec += 1
@@ -98,3 +129,9 @@ while bro < 693:
 print(gogo, "lines processed")
 out.close()
 save.close()
+
+
+
+
+
+
